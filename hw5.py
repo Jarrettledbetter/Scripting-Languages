19bfcc52dfc2e5615ad4063a4fa2d651b30193fd
Jarrettledbetter <Jarrettledbetter@gmail.com>
2022-10-16 22:02:37 -0500

    hw5

---
+++ hw5.py
@@ -0,0 +1,15 @@
+usernames = ['admin', 'Jarrett', 'Greg', 'Ryan', 'Pat', 'Andy']
+for x in usernames:
+    if x == 'admin':
+        print("Hello admin, would you like to see a status report?")
+    else :
+        print( x + " thank you for login in again")
+
+fruits = {'Apples', 'Bananas', 'Pineapple'}
+for x in fruits:
+    print('I like ' + x + ' they are one of my favorite fruits')
+print("I like the sweet flavor of the fruits.")
+print('I also like the tangy taste.')
+print('I like the resfreshing flavor of fruits')
+print('I really love fruits')
+
